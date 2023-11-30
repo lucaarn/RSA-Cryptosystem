b41950451af4c4555d90e468856756d6d2a756d0
Luca Arnecke <luca@arnecke.name>
2023-11-30 18:06:35 +0100

    start of RSAEP

--- Cryptography.hs
+++ Cryptography.hs
@@ -4,3 +4,6 @@ import Transcoding
 
 octetStreamToIntPrimitive :: [String] -> Int
 octetStreamToIntPrimitive em = hexToDec (concat em)
+
+rsaEncryptionPrimitive :: (Int, Int) -> Int -> Int
+rsaEncryptionPrimitive (n, e) m = m
--- Math.hs
+++ Math.hs
@@ -8,17 +8,17 @@ phi p q = (p-1) * (q-1)
 
 -- berechnet das multiplikative Inverse der Zahl x in der modulo-Umgebung m
 mulInverse :: Int -> Int -> Int
-mulInverse x m = mod (fst(erwEuklAlgo x m)) m
+mulInverse x m = mod (fst(extEucAlgo x m)) m
 
 -- Nutzung des erweiterten euklidischen Algorithmus zur bestimmung von (x,y) auf Basis der Gleichung:
 -- ax + by = GGT(a,b)
-erwEuklAlgo :: Int -> Int -> (Int, Int)
-erwEuklAlgo a b
+extEucAlgo :: Int -> Int -> (Int, Int)
+extEucAlgo a b
         | b == 0 = (1, 0)
         | otherwise = (aNew, bNew)
         where aNew = snd temp
               bNew = fst temp - div a b * snd temp
-              temp = erwEuklAlgo b (mod a b)
+              temp = extEucAlgo b (mod a b)
 
 calcKeyLength :: (Int, Int) -> Int
 calcKeyLength (_, n) = floor $ logBase 2 (fromIntegral n :: Double) + 1
